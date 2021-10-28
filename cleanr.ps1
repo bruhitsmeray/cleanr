70745c70d73f94181d8e5c629f0399f4d3ba6fdb
Whiteline <48377708+xwhiteex@users.noreply.github.com>
2021-10-28 18:12:28 +0300

    Update cleanr.ps1

--- cleanr.ps1
+++ cleanr.ps1
@@ -10,8 +10,32 @@ Get-AppxPackage -AllUsers *windowscamera* | Remove-AppxPackage
 # Cortana
 Get-AppxPackage -AllUsers Microsoft.549981C3F5F10 | Remove-AppxPackage
 
+# Calendar & Mail
+Get-AppxPackage -AllUsers *windowscommunicationsapps* | Remove-AppxPackage
+
+# Groove Music
+Get-AppxPackage -AllUsers *zunemusic* | Remove-AppxPackage
+
 # Maps
 Get-AppxPackage -AllUsers *windowsmaps* | Remove-AppxPackage
+
+# Money
+Get-AppxPackage -AllUsers *bingfinance* | Remove-AppxPackage
+
+# Movies
+Get-AppxPackage -AllUsers *zunevideo* | Remove-AppxPackage
+
+# News
+Get-AppxPackage -AllUsers *bingnews* | Remove-AppxPackage
+
+# People
+Get-AppxPackage -AllUsers *people* | Remove-AppxPackage
+
+# Voice Recorder
+Get-AppxPackage -AllUsers *soundrecorder* | Remove-AppxPackage
+
+# Weather
+Get-AppxPackage -AllUsers *bingweather* | Remove-AppxPackage
 
 # Your Phone
 Get-AppxPackage -AllUsers Microsoft.YourPhone | Remove-AppxPackage
