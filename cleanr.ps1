ccaa2eb74ea1d86ab1a2b04d857d83b1917ca4e3
Whiteline <48377708+xwhiteex@users.noreply.github.com>
2021-10-28 19:30:32 +0300

    Update cleanr.ps1

--- cleanr.ps1
+++ cleanr.ps1
@@ -2,40 +2,80 @@ Write-Host "whiteex's Cleanr"
 Write-Host "This script will remove most of the Windows built-in apps."
 
 # Alarms & Clock
-Get-AppxPackage -AllUsers *windowsalarms* | Remove-AppxPackage
+$confirm00 = Read-Host "Do you wish to uninstall Alarms & Clock from your computer? (y/n)"
+if($confirm00 -eq "y") {
+    Get-AppxPackage -AllUsers *windowsalarms* | Remove-AppxPackage
+}
 
 # Camera
-Get-AppxPackage -AllUsers *windowscamera* | Remove-AppxPackage
+$confirm01 = Read-Host "Do you wish to uninstall Camera from your computer? (y/n)"
+if($confirm01 -eq "y") {
+    Get-AppxPackage -AllUsers *windowscamera* | Remove-AppxPackage
+}
 
 # Cortana
-Get-AppxPackage -AllUsers Microsoft.549981C3F5F10 | Remove-AppxPackage
+$confirm02 = Read-Host "Do you wish to uninstall Cortana from your computer? (y/n)"
+if($confirm02 -eq "y") {
+    Get-AppxPackage -AllUsers Microsoft.549981C3F5F10 | Remove-AppxPackage
+}
 
 # Calendar & Mail
-Get-AppxPackage -AllUsers *windowscommunicationsapps* | Remove-AppxPackage
+$confirm03 = Read-Host "Do you wish to uninstall Calendar & Mail from your computer? (y/n)"
+if($confirm03 -eq "y") {
+    Get-AppxPackage -AllUsers *windowscommunicationsapps* | Remove-AppxPackage
+}
 
 # Groove Music
-Get-AppxPackage -AllUsers *zunemusic* | Remove-AppxPackage
+$confirm04 = Read-Host "Do you wish to uninstall Groove Music from your computer? (y/n)"
+if($confirm04 -eq "y") {
+    Get-AppxPackage -AllUsers *zunemusic* | Remove-AppxPackage
+}
 
 # Maps
-Get-AppxPackage -AllUsers *windowsmaps* | Remove-AppxPackage
+$confirm05 = Read-Host "Do you wish to uninstall Maps from your computer? (y/n)"
+if($confirm05 -eq "y") {
+    Get-AppxPackage -AllUsers *windowsmaps* | Remove-AppxPackage
+}
 
 # Money
-Get-AppxPackage -AllUsers *bingfinance* | Remove-AppxPackage
+$confirm06 = Read-Host "Do you wish to uninstall Money from your computer? (y/n)"
+if($confirm06 -eq "y") {
+    Get-AppxPackage -AllUsers *bingfinance* | Remove-AppxPackage
+}
 
 # Movies
-Get-AppxPackage -AllUsers *zunevideo* | Remove-AppxPackage
+$confirm07 = Read-Host "Do you wish to uninstall Movies from your computer? (y/n)"
+if($confirm07 -eq "y") {
+    Get-AppxPackage -AllUsers *zunevideo* | Remove-AppxPackage
+}
 
 # News
-Get-AppxPackage -AllUsers *bingnews* | Remove-AppxPackage
+$confirm08 = Read-Host "Do you wish to uninstall News from your computer? (y/n)"
+if($confirm08 -eq "y") {
+    Get-AppxPackage -AllUsers *bingnews* | Remove-AppxPackage
+
+}
 
 # People
-Get-AppxPackage -AllUsers *people* | Remove-AppxPackage
+$confirm09 = Read-Host "Do you wish to uninstall People from your computer? (y/n)"
+if($confirm09 -eq "y") {
+    Get-AppxPackage -AllUsers *people* | Remove-AppxPackage
+}
 
 # Voice Recorder
-Get-AppxPackage -AllUsers *soundrecorder* | Remove-AppxPackage
+$confirm10 = Read-Host "Do you wish to uninstall Voice Recorder from your computer? (y/n)"
+if($confirm10 -eq "y") {
+    Get-AppxPackage -AllUsers *soundrecorder* | Remove-AppxPackage
+}
 
 # Weather
-Get-AppxPackage -AllUsers *bingweather* | Remove-AppxPackage
+$confirm11 = Read-Host "Do you wish to uninstall Weather from your computer? (y/n)"
+if($confirm11 -eq "y") {
+    Get-AppxPackage -AllUsers *bingweather* | Remove-AppxPackage
+}
 
 # Your Phone
-Get-AppxPackage -AllUsers Microsoft.YourPhone | Remove-AppxPackage
+$confirm12 = Read-Host "Do you wish to uninstall Your Phone from your computer? (y/n)"
+if($confirm12 -eq "y") {
+    Get-AppxPackage -AllUsers Microsoft.YourPhone | Remove-AppxPackage
+}
